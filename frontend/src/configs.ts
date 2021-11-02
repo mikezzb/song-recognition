bbeb79b37e6c2dfc65e191370520cf8ea48187a6
mikezzb <mikezhoudev@gmail.com>
2021-11-02 22:13:05 +0800

    [frontend] Add mic recorder

--- frontend/src/configs.ts
+++ frontend/src/configs.ts
@@ -1,5 +1,10 @@
 import { AxiosRequestConfig } from 'axios';
 
+// Main
+export const RECORDER_BIT_RATE = 128;
+export const RECORD_SECONDS = 6;
+
+// Axios
 export const SERVER_ADDRESS =
   process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4000';
 
--- frontend/src/containers/HomePage.tsx
+++ frontend/src/containers/HomePage.tsx
@@ -1,3 +1,39 @@
-const HomePage = () => <div>hiii</div>;
+import { useState } from 'react';
+import MicRecorder from 'mic-recorder-to-mp3';
+import { RECORDER_BIT_RATE, RECORD_SECONDS } from '../configs';
+import './HomePage.scss';
+
+const recorder = new MicRecorder({ bitRate: RECORDER_BIT_RATE });
+
+const HomePage = () => {
+  const [recording, setRecording] = useState(false);
+  const [audioURL, setAudioURL] = useState(null);
+  const record = async () => {
+    try {
+      await recorder.start();
+      setRecording(true);
+      await new Promise(resolve => setTimeout(resolve, RECORD_SECONDS * 1000));
+      setRecording(false);
+      const [buffer, blob] = await recorder.stop().getMp3();
+      const file = new File(buffer, 'me-at-thevoice.mp3', {
+        type: blob.type,
+        lastModified: +new Date(),
+      });
+      setAudioURL(URL.createObjectURL(file));
+    } catch (e) {
+      alert(e);
+    }
+  };
+  return (
+    <div className="homepage">
+      {!recording && <button onClick={record}>Record</button>}
+      {Boolean(audioURL) && (
+        <audio controls>
+          <source src={audioURL} type="audio/mpeg" />
+        </audio>
+      )}
+    </div>
+  );
+};
 
 export default HomePage;
