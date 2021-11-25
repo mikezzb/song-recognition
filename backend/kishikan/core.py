[qbh] Fix dtw calculation

backend/kishikan/core.py
@@ -3,7 +3,6 @@ import hashlib
 import librosa
 from typing import List
 import numpy as np
-import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from skimage.feature import peak_local_max
 from kishikan.types import Fingerprint
@@ -36,6 +35,7 @@ def _get_img_peaks(im: np.ndarray, verbose: bool):
     # print(amps.shape)
     # print(peaks.shape)
     peaks = peaks[amps > AMP_MIN]
+    """
     max_peaks = defaultdict(lambda: {
         "max_amp": 0,
     })
@@ -47,6 +47,7 @@ def _get_img_peaks(im: np.ndarray, verbose: bool):
     peaks = np.array([d["peak"] for d in max_peaks.values()])
     # print(peaks)
     # plt the peaks
+    """
     if verbose:
         print(f'Detected peaks {peaks.shape}')
         plt.figure(figsize=(14, 14))
@@ -74,7 +75,6 @@ def _fingerprint_hashes(peaks: np.ndarray) -> List[Fingerprint]:
             if (i + j) < n:
                 t1 = peaks[i][TIME_INDEX]
                 t2 = peaks[i + j][TIME_INDEX]
-                print(f"{t1} {t2}")
                 t_delta = t2 - t1
                 if t_delta >= MIN_HASH_TIME_DELTA and t_delta <= MAX_HASH_TIME_DELTA:  
                     f1 = peaks[i][FREQ_INDEX]

backend/nazo/core.py
@@ -3,6 +3,9 @@ import numpy as np
 import librosa
 from nazo.configs import F_MAX, F_MIN, FRAME_LENGTH, PITCHES_PER_SECOND
 
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+
 def convert_to_midi():
     pass
 
@@ -36,7 +39,7 @@ def pitches_to_series(pitches: np.ndarray):
         if carry > 1:
             num_pvs += 1
             carry -= 1
-        print(f"{duration}: {num_pvs}")
+        # print(f"{duration}: {num_pvs}")
         note_pvs = np.full(num_pvs, pitch, dtype=np.float)
         series = np.concatenate([series, note_pvs]) if series is not None else note_pvs
     return series
@@ -47,9 +50,23 @@ def pv_to_time_series(file):
     return ts
 
 def audio_to_pitches(y, sr):
+    # num_pitches = int(librosa.get_duration(y, sr=sr) * PITCHES_PER_SECOND)
+    # print(num_pitches)
     f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=F_MIN, fmax=F_MAX, fill_na=np.nan, frame_length=FRAME_LENGTH)
+    # print(len(f0))
     return librosa.hz_to_midi(f0[~np.isnan(f0)])
 
+
+A = 0.8
+B = 0.6
+
 def score(x: np.ndarray, y: np.ndarray):
-    D, wp = librosa.sequence.dtw(x, y, subseq=True, backtrack=True)
-    return np.min(D[-1, :] / wp.shape[0])
+    n = x.shape[0]
+    m = y.shape[0]
+    new_len = min(n, m)
+    x, y = x[:new_len], y[:new_len]
+    d_mean = np.mean(x) - np.mean(y)
+    x -= d_mean
+    e, _ = fastdtw(x, y, radius=2, dist=euclidean)
+    score = A * B ** (e / n) + (1 - A) * (m / n)
+    return e