985f62825d539f5f5a1cd403ff2e35fcc8dd5b33
mikezzb <mikezhoudev@gmail.com>
2021-12-20 13:14:13 +0800

    [server] Fix audio streaming

--- backend/app/routes/player.py
+++ backend/app/routes/player.py
@@ -5,10 +5,16 @@ from flask_restful import Resource, reqparse
 from app.configs import SONGS_DIR_NAME
 
 dir = os.path.dirname(__file__)
-songs_dir = os.path.join(dir, f'../../{SONGS_DIR_NAME}')
+songs_dir = os.path.join(dir, f'../../../{SONGS_DIR_NAME}')
 
 parser = reqparse.RequestParser()
 
+def generate(filename):
+    with open(f"{songs_dir}/{filename}", "rb") as f:
+        data = f.read(1024)
+        while data:
+            yield data
+            data = f.read(1024)
 class Player(Resource):
     def get(self, filename):
-        return Response(wave.open(f"{songs_dir}/{filename}", "r"), mimetype="audio/x-wav")
+        return Response(generate(filename), mimetype="audio/mpeg")
